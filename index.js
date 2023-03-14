08ae3d4d319347baff327e9d287abfd6af4e4890
Lixel Amos <amosbett99@gmail.com>
2023-03-14 10:04:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+'Grace Hopper'.charAt(0);
+//=> "G"
+'Grace Hopper'.slice();
+//=> "G
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
